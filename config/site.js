62fc9a44c69d8f4342375f96b007996cc381b7ac
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-04-09 11:54:08 +0900

    🎨 language setting set to Japanese

--- config/site.js
+++ config/site.js
@@ -13,9 +13,9 @@ module.exports = {
   description: "gatsby version of startbootstrap-agency with i18n support.",
   author: "thundermiracle",
   trackingId: "UA-119418003-5",
-  defaultLang: "en",
+  defaultLang: "ja",
   langTextMap: {
-    en: "English",
     ja: "日本語",
+    en: "English",
   },
 };
