df2ae72e5e73dbc2a6c554486322c0b9c44ccbd0
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-08-10 17:31:14 +0900

    ⏪ YouTubeに戻す

--- src/views/Sections/Services/firstConcert.js
+++ src/views/Sections/Services/firstConcert.js
@@ -9,10 +9,11 @@ const FirstConcert = () => {
       <div style={{ marginTop: "5rem", marginBottom: "5rem" }}>
         <iframe
           title="ポレポーレ第一回演奏会ご案内"
-          src="https://drive.google.com/file/d/1sdCRmOcvlV1S4YEU24VTZLEtaz0Az5lx/preview"
           width="100%"
           height="615px"
-          allow="autoplay"
+          src="https://www.youtube.com/embed/rj49jZymIcM"
+          frameBorder="0"
+          allowFullScreen
         />
       </div>
     </Container>
