91f75b1d4668c834c8579957359c849124d0ce7f
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2022-04-14 08:19:08 +0900

    twitter image追加してみる

--- config/site.js
+++ config/site.js
@@ -1,5 +1,6 @@
 module.exports = {
   title: "ポレポーレ",
+  image: "https://polepole.netlify.app/images/seo.jpg",
   keywords: [
     "gatsby",
     "gatsbyjs",
@@ -8,14 +9,15 @@ module.exports = {
     "女声コーラス",
     "神奈川県",
     "合唱",
-    "startbootstrap-agency",
+    // "startbootstrap-agency",
   ],
-  description: "ポレポーレは、座間市で活動する女声合唱団です。ポレポーレとはスワヒリ語でゆっくりのんびりという意味だそうです。ゆっくりのんびりと互いを信頼し合いながら、楽しく歌っています。",
+  description:
+    "ポレポーレは、座間市で活動する女声合唱団です。ポレポーレとはスワヒリ語でゆっくりのんびりという意味だそうです。ゆっくりのんびりと互いを信頼し合いながら、楽しく歌っています。",
   author: "ポレポーレ created by yoko",
   trackingId: "UA-119418003-5",
   defaultLang: "ja",
   langTextMap: {
     ja: "日本語",
-    en: "English",
+    // en: "English",
   },
 };
